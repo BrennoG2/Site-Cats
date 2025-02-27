05f6213d9f04e1d3a541f17e88cf85c4d2c971d2
BrennoG2 <bngoncalvesj@gmail.com>
2025-02-27 15:59:53 -0300

    Update dados.php

--- dados.php
+++ dados.php
@@ -1,2 +1,29 @@
 <?php
-$nome = $_POST['catphotourl']
+$link = $_POST['catphotourl']
+$data = date('d/m/y);
+$hora = date('H:i:s');
+
+
+$server = 'localhost';
+$user = 'root';
+$password = '';
+$dados = 'catss'
+
+$conn = new mysqli($server, $user, $password, $dados);
+
+if($conn->connect_error){
+  die("deu ruim".$conn->connect_error);
+}
+
+$smtp = $conn->prepare("INSERT INTO cats (link, data, hora) VALUES (?,?,?)");
+$smtp->bind_param("sss",$link, $data, $hora)
+
+if($smtp->execute()){
+  echo "Foi"
+}else{
+  echo "deu ruim".$smtp->error;
+}
+
+$smpt->close();
+$conn->close();
+?>
